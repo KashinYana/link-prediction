407d53c8a9e7480c4d12a5422d7f78a1e7dd8994
Ubuntu <ubuntu@ip-172-31-18-171.us-west-2.compute.internal>
2017-03-28 08:02:27 +0000

    add graph tool patch

---
+++ graph_tools_patch/graph_sfdp.cc
@@ -0,0 +1,262 @@
+// graph-tool -- a general graph modification and manipulation thingy
+//
+// Copyright (C) 2006-2017 Tiago de Paula Peixoto <tiago@skewed.de>
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 3
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#include "graph_filtering.hh"
+#include "graph.hh"
+#include "graph_properties.hh"
+#include "graph_exceptions.hh"
+
+#include <boost/lambda/bind.hpp>
+
+#include "graph_sfdp.hh"
+#include "random.hh"
+#include "hash_map_wrap.hh"
+
+using namespace std;
+using namespace boost;
+using namespace graph_tool;
+
+void sfdp_layout(GraphInterface& g, boost::any pos, boost::any vweight,
+                 boost::any eweight, boost::any pin, python::object spring_parms,
+                 double theta, double init_step, double step_schedule,
+                 size_t max_level, double epsilon, size_t max_iter,
+                 bool adaptive, bool verbose, bool bipartite, rng_t& rng)
+{
+    typedef UnityPropertyMap<int,GraphInterface::vertex_t> vweight_map_t;
+    typedef UnityPropertyMap<int,GraphInterface::edge_t> eweight_map_t;
+    typedef mpl::push_back<vertex_scalar_properties, vweight_map_t>::type
+        vertex_props_t;
+    typedef mpl::push_back<edge_scalar_properties, eweight_map_t>::type
+        edge_props_t;
+
+    typedef vprop_map_t<int32_t>::type group_map_t;
+
+    double C = python::extract<double>(spring_parms[0]);
+    double K = python::extract<double>(spring_parms[1]);
+    double p = python::extract<double>(spring_parms[2]);
+    double gamma = python::extract<double>(spring_parms[3]);
+    double mu = python::extract<double>(spring_parms[4]);
+    double mu_p = python::extract<double>(spring_parms[5]);
+    group_map_t groups =
+        any_cast<group_map_t>(python::extract<any>(spring_parms[6]));
+
+    if(vweight.empty())
+        vweight = vweight_map_t();
+    if(eweight.empty())
+        eweight = eweight_map_t();
+
+    typedef vprop_map_t<uint8_t>::type pin_map_t;
+    pin_map_t pin_map = any_cast<pin_map_t>(pin);
+
+    run_action<graph_tool::detail::never_directed>()
+        (g,
+         std::bind(get_sfdp_layout(C, K, p, theta, gamma, mu, mu_p, init_step,
+                                   step_schedule, max_level, epsilon,
+                                   max_iter, adaptive, bipartite),
+                   std::placeholders::_1, std::placeholders::_2,
+                   std::placeholders::_3, std::placeholders::_4,
+                   pin_map.get_unchecked(num_vertices(g.get_graph())),
+                   groups.get_unchecked(num_vertices(g.get_graph())), verbose,
+                   std::ref(rng)),
+         vertex_floating_vector_properties(), vertex_props_t(), edge_props_t())
+        (pos, vweight, eweight);
+}
+
+struct do_propagate_pos
+{
+    template <class Graph, class CoarseGraph, class VertexMap, class PosMap,
+              class RNG>
+    void operator()(Graph& g, CoarseGraph& cg, VertexMap vmap,
+                    boost::any acvmap, PosMap pos, boost::any acpos,
+                    double delta, RNG& rng) const
+    {
+        typename PosMap::checked_t cpos =
+            any_cast<typename PosMap::checked_t>(acpos);
+        typename VertexMap::checked_t cvmap =
+            any_cast<typename VertexMap::checked_t>(acvmap);
+        typedef typename property_traits<VertexMap>::value_type c_t;
+        typedef typename property_traits<PosMap>::value_type pos_t;
+        typedef typename pos_t::value_type val_t;
+
+        uniform_real_distribution<val_t> noise(-delta, delta);
+        gt_hash_map<c_t, pos_t> cmap;
+
+        for (auto v : vertices_range(cg))
+            cmap[cvmap[v]] = cpos[v];
+
+        for (auto v : vertices_range(g))
+        {
+            pos[v] = cmap[vmap[v]];
+
+            if (delta > 0)
+            {
+                for (size_t j = 0; j < pos[v].size(); ++j)
+                    pos[v][j] += noise(rng);
+            }
+        }
+    }
+};
+
+void propagate_pos(GraphInterface& gi, GraphInterface& cgi, boost::any vmap,
+                   boost::any cvmap, boost::any pos, boost::any cpos,
+                   double delta, rng_t& rng)
+{
+    typedef mpl::vector<property_map_type::apply
+                            <int32_t,
+                             GraphInterface::vertex_index_map_t>::type>::type
+        vmaps_t;
+
+    gt_dispatch<>()
+        (std::bind(do_propagate_pos(),
+                       std::placeholders::_1, std::placeholders::_2, std::placeholders::_3,
+                       cvmap, std::placeholders::_4, cpos, delta, std::ref(rng)),
+         all_graph_views(), all_graph_views(),
+         vmaps_t(), vertex_floating_vector_properties())
+        (gi.get_graph_view(), cgi.get_graph_view(), vmap, pos);
+}
+
+struct do_propagate_pos_mivs
+{
+    template <class Graph, class MIVSMap, class PosMap,
+              class RNG>
+    void operator()(Graph& g, MIVSMap mivs, PosMap pos, double delta, RNG& rng) const
+    {
+        typedef typename property_traits<PosMap>::value_type pos_t;
+        typedef typename pos_t::value_type val_t;
+
+        uniform_real_distribution<val_t> noise(-delta, delta);
+
+        for (auto v : vertices_range(g))
+        {
+            if (mivs[v])
+                continue;
+            size_t count = 0;
+            for (auto a : adjacent_vertices_range(v, g))
+            {
+                if (!mivs[a])
+                    continue;
+                pos[v].resize(pos[a].size(), 0);
+                for (size_t j = 0; j < pos[a].size(); ++j)
+                    pos[v][j] += pos[a][j];
+                ++count;
+            }
+
+            if (count == 0)
+                throw ValueException("invalid MIVS! Vertex has no neighbours "
+                                     "belonging to the set!");
+
+            if (count == 1)
+            {
+                if (delta > 0)
+                {
+                    for (size_t j = 0; j < pos[v].size(); ++j)
+                        pos[v][j] += noise(rng);
+                }
+            }
+            else
+            {
+                for (size_t j = 0; j < pos[v].size(); ++j)
+                    pos[v][j] /= count;
+            }
+        }
+    }
+};
+
+
+void propagate_pos_mivs(GraphInterface& gi, boost::any mivs, boost::any pos,
+                        double delta, rng_t& rng)
+{
+    run_action<>()
+        (gi, std::bind(do_propagate_pos_mivs(),
+                       std::placeholders::_1, std::placeholders::_2, std::placeholders::_3,
+                       delta, std::ref(rng)),
+         vertex_scalar_properties(), vertex_floating_vector_properties())
+        (mivs, pos);
+}
+
+
+struct do_avg_dist
+{
+    template <class Graph, class PosMap>
+    void operator()(Graph& g, PosMap pos, double& ad) const
+    {
+        size_t count = 0;
+        double d = 0;
+        #pragma omp parallel if (num_vertices(g) > OPENMP_MIN_THRESH) \
+            reduction(+: d, count)
+        parallel_vertex_loop_no_spawn
+                (g,
+                 [&](auto v)
+                 {
+                     for (auto a : adjacent_vertices_range(v, g))
+                     {
+                         d += dist(pos[v], pos[a]);
+                         count++;
+                     }
+                 });
+        if (count > 0)
+            d /= count;
+        ad = d;
+    }
+};
+
+
+double avg_dist(GraphInterface& gi, boost::any pos)
+{
+    double d;
+    run_action<>()
+        (gi, std::bind(do_avg_dist(), std::placeholders::_1,
+                       std::placeholders::_2, std::ref(d)),
+         vertex_scalar_vector_properties()) (pos);
+    return d;
+}
+
+
+struct do_sanitize_pos
+{
+    template <class Graph, class PosMap>
+    void operator()(Graph& g, PosMap pos) const
+    {
+        parallel_vertex_loop
+                (g,
+                 [&](auto v)
+                 {
+                     pos[v].resize(2);
+                 });
+    }
+};
+
+
+void sanitize_pos(GraphInterface& gi, boost::any pos)
+{
+    run_action<>()
+        (gi, std::bind(do_sanitize_pos(), std::placeholders::_1, std::placeholders::_2),
+         vertex_scalar_vector_properties()) (pos);
+}
+
+#include <boost/python.hpp>
+
+void export_sfdp()
+{
+    python::def("sfdp_layout", &sfdp_layout);
+    python::def("propagate_pos", &propagate_pos);
+    python::def("propagate_pos_mivs", &propagate_pos_mivs);
+    python::def("avg_dist", &avg_dist);
+    python::def("sanitize_pos", &sanitize_pos);
+}
+
+
---
+++ graph_tools_patch/graph_sfdp.hh
@@ -0,0 +1,506 @@
+fndef GRAPH_FDP_HH
+#define GRAPH_FDP_HH
+
+#include <limits>
+#include <iostream>
+
+#ifndef __clang__
+#include <ext/numeric>
+using __gnu_cxx::power;
+#else
+template <class Value>
+Value power(Value value, int n)
+{
+    return pow(value, n);
+}
+#endif
+
+namespace graph_tool
+{
+using namespace std;
+using namespace boost;
+
+template <class Pos, class Weight>
+class QuadTree
+{
+public:
+    QuadTree(const Pos& ll, const Pos& ur, int max_level)
+        :_ll(ll), _ur(ur), _cm(2, 0), _count(0),
+         _max_level(max_level)
+    {
+        _w = sqrt(power(_ur[0] - _ll[0], 2) +
+                  power(_ur[1] - _ll[1], 2));
+    }
+
+    vector<QuadTree>& get_leafs()
+    {
+        if (_max_level > 0 && _leafs.empty())
+        {
+            _leafs.reserve(4);
+            for (size_t i = 0; i < 4; ++i)
+            {
+                Pos lll = _ll, lur = _ur;
+                if (i % 2)
+                    lll[0] += (_ur[0] - _ll[0]) / 2;
+                else
+                    lur[0] -= (_ur[0] - _ll[0]) / 2;
+                if (i / 2)
+                    lll[1] += (_ur[1] - _ll[1]) / 2;
+                else
+                    lur[1] -= (_ur[1] - _ll[1]) / 2;
+                _leafs.emplace_back(lll, lur, _max_level - 1);
+            }
+        }
+
+        return _leafs;
+    }
+
+    vector<std::tuple<Pos,Weight> >& get_dense_leafs()
+    {
+        return _dense_leafs;
+    }
+
+    size_t get_branch(Pos& p)
+    {
+        int i = p[0] > (_ll[0] + (_ur[0] - _ll[0]) / 2);
+        int j = p[1] > (_ll[1] + (_ur[1] - _ll[1]) / 2);
+        return i + 2 * j;
+    }
+
+    size_t put_pos(Pos& p, Weight w)
+    {
+        _count += w;
+        _cm[0] += p[0] * w;
+        _cm[1] += p[1] * w;
+
+        if (_max_level == 0 || _count == w)
+        {
+            _dense_leafs.emplace_back(p, w);
+            return 1;
+        }
+        else
+        {
+            if (!_dense_leafs.empty())
+            {
+                // move dense leafs down
+                auto& leafs = get_leafs();
+                for (auto& leaf : _dense_leafs)
+                {
+                    auto& lp = get<0>(leaf);
+                    auto& lw = get<1>(leaf);
+                    leafs[get_branch(lp)].put_pos(lp, lw);
+                }
+                _dense_leafs.clear();
+            }
+
+            size_t sc = (_max_level > 0 && _leafs.empty()) ? 4 : 0;
+            return sc + get_leafs()[get_branch(p)].put_pos(p, w);
+        }
+        return 0;
+    }
+
+    void get_cm(Pos& cm)
+    {
+        for (size_t i = 0; i < 2; ++i)
+            cm[i] = _cm[i] / _count;
+    }
+
+    double get_w() const
+    {
+        return _w;
+    }
+
+    Weight get_count()
+    {
+        return _count;
+    }
+
+    int max_level()
+    {
+        return _max_level;
+    }
+
+private:
+    Pos _ll, _ur;
+    vector<QuadTree> _leafs;
+    vector<std::tuple<Pos,Weight> > _dense_leafs;
+    Pos _cm;
+    Weight _count;
+    int _max_level;
+    double _w;
+};
+
+template <class Pos>
+inline double dist(const Pos& p1, const Pos& p2)
+{
+    double r = 0;
+    for (size_t i = 0; i < 2; ++i)
+        r += power(double(p1[i] - p2[i]), 2);
+    return sqrt(r);
+}
+
+template <class Pos>
+inline double f_r(double C, double K, double p, const Pos& p1, const Pos& p2)
+{
+    double d = dist(p1, p2);
+    if (d == 0)
+        return 0;
+    if (round(p) == p)
+        return -C * power(K, int(1 + p)) / power(d, int(p));
+    else
+        return -C * pow(K, 1 + p) / pow(d, p);
+}
+
+template <class Pos>
+inline double f_a(double K, const Pos& p1, const Pos& p2)
+{
+    return power(dist(p1, p2), 2) / K;
+}
+
+template <class Pos>
+inline double get_diff(const Pos& p1, const Pos& p2, Pos& r, double d=0)
+{
+    double abs = 0;
+    for (size_t i = 0; i < 2; ++i)
+    {
+        r[i] = p1[i] - p2[i];
+        abs += r[i] * r[i];
+    }
+    abs += d*d;
+    if (abs == 0)
+        abs = 1;
+    abs = sqrt(abs);
+    for (size_t i = 0; i < 2; ++i)
+        r[i] /= abs;
+    return abs;
+}
+
+template <class Pos>
+inline double norm(Pos& x)
+{
+    double abs = 0;
+    for (size_t i = 0; i < 2; ++i)
+        abs += power(x[i], 2);
+    for (size_t i = 0; i < 2; ++i)
+        x[i] /= sqrt(abs);
+    return sqrt(abs);
+}
+
+struct get_sfdp_layout
+{
+    get_sfdp_layout(double C, double K, double p, double theta, double gamma,
+                    double mu, double mu_p, double init_step,
+                    double step_schedule, size_t max_level, double epsilon,
+                    size_t max_iter, bool simple, bool bipartite)
+        : C(C), K(K), p(p), theta(theta), gamma(gamma), mu(mu), mu_p(mu_p),
+          init_step(init_step), step_schedule(step_schedule),
+          epsilon(epsilon), max_level(max_level), max_iter(max_iter),
+          simple(simple), bipartite(bipartite) {}
+
+    double C, K, p, theta, gamma, mu, mu_p, init_step, step_schedule, epsilon;
+    size_t max_level, max_iter;
+    bool simple, bipartite;
+
+    template <class Graph, class PosMap, class VertexWeightMap,
+              class EdgeWeightMap, class PinMap, class GroupMap, class RNG>
+    void operator()(Graph& g, PosMap pos, VertexWeightMap vweight,
+                    EdgeWeightMap eweight, PinMap pin, GroupMap group,
+                    bool verbose, RNG& rng) const
+    {
+        typedef typename property_traits<PosMap>::value_type pos_t;
+        typedef typename property_traits<PosMap>::value_type::value_type val_t;
+
+        typedef typename property_traits<VertexWeightMap>::value_type vweight_t;
+
+        vector<pos_t> group_cm;
+        vector<vweight_t> group_size;
+        vector<size_t> vertices;
+
+        int HN = 0;
+        for (auto v : vertices_range(g))
+        {
+            if (pin[v] == 0)
+                vertices.push_back(v);
+            pos[v].resize(2, 0);
+            if (gamma != 0 || mu != 0)
+            {
+                size_t s = group[v];
+
+                if (s >= group_cm.size())
+                {
+                    group_cm.resize(s + 1);
+                    group_size.resize(s + 1, 0);
+                }
+                group_cm[s].resize(2, 0);
+                group_size[s] += get(vweight, v);
+
+                for (size_t j = 0; j < 2; ++j)
+                    group_cm[s][j] += pos[v][j] * get(vweight, v);
+            }
+            HN++;
+        }
+
+        for (size_t s = 0; s < group_size.size(); ++s)
+        {
+            if (group_size[s] == 0)
+                continue;
+            group_cm[s].resize(2, 0);
+            for (size_t j = 0; j < 2; ++j)
+                group_cm[s][j] /= group_size[s];
+        }
+
+        val_t delta = epsilon * K + 1, E = 0, E0;
+        E0 = numeric_limits<val_t>::max();
+        size_t n_iter = 0;
+        val_t step = init_step;
+        size_t progress = 0;
+
+        while (delta > epsilon * K && (max_iter == 0 || n_iter < max_iter))
+        {
+            delta = 0;
+            E0 = E;
+            E = 0;
+
+            pos_t ll(2, numeric_limits<val_t>::max()),
+                ur(2, -numeric_limits<val_t>::max());
+            for (auto v : vertices_range(g))
+            {
+                for (size_t j = 0; j < 2; ++j)
+                {
+                    ll[j] = min(pos[v][j], ll[j]);
+                    ur[j] = max(pos[v][j], ur[j]);
+                }
+            }
+
+            if (gamma != 0 || mu != 0)
+            {
+                for (size_t s = 0; s < group_size.size(); ++s)
+                {
+                    if (group_size[s] == 0)
+                        continue;
+                    group_cm[s] = {0, 0};
+                }
+
+                for (auto v : vertices_range(g))
+                {
+                    size_t s = group[v];
+                    for (size_t j = 0; j < 2; ++j)
+                        group_cm[s][j] += pos[v][j] * get(vweight, v) /
+                            group_size[s];
+                }
+            }
+
+            QuadTree<pos_t, vweight_t> qt(ll, ur, max_level);
+            
+            QuadTree<pos_t, vweight_t> qtl(ll, ur, max_level);
+            QuadTree<pos_t, vweight_t> qtr(ll, ur, max_level);
+            
+            if (!bipartite) {
+                
+                if (verbose)
+                    cout << "Bipartite is off" << endl;
+                
+                for (auto v : vertices_range(g))
+                    qt.put_pos(pos[v], vweight[v]);
+            } else {
+                
+                if (verbose)
+                    cout << "Bipartite is on" << endl;
+                
+                for (auto v : vertices_range(g)) {
+                    if (size_t(group[v])) {
+                        qtl.put_pos(pos[v], vweight[v]);
+                    } else {
+                        qtr.put_pos(pos[v], vweight[v]);
+                    }
+                }
+            }
+            
+            std::shuffle(vertices.begin(), vertices.end(), rng);
+
+            size_t nmoves = 0;
+            vector<QuadTree<pos_t, vweight_t>*> Q;
+
+            #pragma omp parallel if (num_vertices(g) > OPENMP_MIN_THRESH) \
+                private(Q) reduction(+:E, delta, nmoves)
+            parallel_loop_no_spawn
+                (vertices,
+                 [&](size_t, auto v)
+                 {
+                     pos_t diff(2, 0), pos_u(2, 0), ftot(2, 0), cm(2, 0);
+                     double d;
+                     // global repulsive forces
+                     if (!bipartite) {
+                         Q.push_back(&qt);
+                         d = 0;
+                     } else {
+                         d = 1;
+                         if (size_t(group[v])) {
+                             Q.push_back(&qtr);
+                         } else {
+                             Q.push_back(&qtl);
+                         }
+                     }
+                     while (!Q.empty())
+                     {
+                         auto& q = *Q.back();
+                         Q.pop_back();
+
+                         auto& dleafs = q.get_dense_leafs();
+                         if (!dleafs.empty())
+                         {
+                             for (auto& dleaf : dleafs)
+                             {
+                                 val_t d = get_diff(get<0>(dleaf), pos[v], diff, d);
+                                 if (d == 0)
+                                     continue;
+                                 val_t f = f_r(C, K, p, pos[v], get<0>(dleaf));
+                                 f *= get<1>(dleaf) * get(vweight, v);
+                                 for (size_t l = 0; l < 2; ++l)
+                                     ftot[l] += f * diff[l];
+                             }
+                         }
+                         else
+                         {
+                             double w = q.get_w();
+                             q.get_cm(cm);
+                             double d = get_diff(cm, pos[v], diff, d);
+                             if (w > theta * d)
+                             {
+                                 for (auto& leaf : q.get_leafs())
+                                 {
+                                     if (leaf.get_count() > 0)
+                                         Q.push_back(&leaf);
+                                 }
+                             }
+                             else
+                             {
+                                 if (d > 0)
+                                 {
+                                     val_t f = f_r(C, K, p, cm, pos[v]);
+                                     f *= q.get_count() * get(vweight, v);
+                                     for (size_t l = 0; l < 2; ++l)
+                                         ftot[l] += f * diff[l];
+                                 }
+                             }
+                         }
+                     }
+
+                     // local attractive forces
+                     auto& pos_v = pos[v];
+                     for (auto e : out_edges_range(v, g))
+                     {
+                         auto u = target(e, g);
+                         if (u == v)
+                             continue;
+                         pos_u = pos[u];
+                         get_diff(pos_u, pos_v, diff, d);
+                         val_t f = f_a(K, pos_u, pos_v);
+                         f *= get(eweight, e) * get(vweight, u) * get(vweight, v);
+                         for (size_t l = 0; l < 2; ++l)
+                             ftot[l] += f * diff[l];
+                     }
+
+                     // inter-group attractive forces
+                     if (gamma > 0)
+                     {
+                         for (size_t s = 0; s < group_cm.size(); ++s)
+                         {
+                             if (group_size[s] == 0)
+                                 continue;
+                             if (s == size_t(group[v]))
+                                 continue;
+                             val_t d = get_diff(group_cm[s], pos[v], diff, d);
+                             if (d == 0)
+                                 continue;
+                             double Kp = K * power(HN, 2);
+                             val_t f = f_a(Kp, group_cm[s], pos[v]) * gamma * \
+                                 group_size[s] * get(vweight, v);
+                             for (size_t l = 0; l < 2; ++l)
+                                 ftot[l] += f * diff[l];
+                         }
+                     }
+
+                     // inter-group repulsive forces
+                     if (gamma < 0)
+                     {
+                         for (size_t s = 0; s < group_cm.size(); ++s)
+                         {
+                             if (group_size[s] == 0)
+                                 continue;
+                             if (s == size_t(group[v]))
+                                 continue;
+                             val_t d = get_diff(group_cm[s], pos[v], diff, d);
+                             if (d == 0)
+                            continue;
+                             val_t f = f_r(C, K, p, cm, pos[v]);
+                             f *= group_size[s] * get(vweight, v) * abs(gamma);
+                             for (size_t l = 0; l < 2; ++l)
+                                 ftot[l] += f * diff[l];
+                         }
+                     }
+
+                     // intra-group attractive forces
+                     if (mu > 0 && group_size[group[v]] > 1)
+                     {
+                         val_t d = get_diff(group_cm[group[v]], pos[v], diff, d);
+                         if (d > 0)
+                         {
+                             double Kp = K * pow(double(group_size[group[v]]), mu_p);
+                             val_t f = f_a(Kp, group_cm[group[v]], pos[v]) * mu * \
+                                 group_size[group[v]] * get(vweight, v);
+                             for (size_t l = 0; l < 2; ++l)
+                                 ftot[l] += f * diff[l];
+                         }
+                     }
+
+                     E += power(norm(ftot), 2);
+
+                     for (size_t l = 0; l < 2; ++l)
+                     {
+                         ftot[l] *= step;
+                         pos[v][l] += ftot[l];
+                     }
+
+                     delta += norm(ftot);
+                     nmoves++;
+                 });
+
+            n_iter++;
+            delta /= nmoves;
+
+            if (verbose)
+                cout << n_iter << " " << E << " " << step << " "
+                     << delta << " " << max_level << endl;
+
+            if (simple)
+            {
+                step *= step_schedule;
+            }
+            else
+            {
+                if (E < E0)
+                {
+                    ++progress;
+                    if (progress >= 5)
+                    {
+                        progress = 0;
+                        step /= step_schedule;
+                    }
+                }
+                else
+                {
+                    progress = 0;
+                    step *= step_schedule;
+                }
+            }
+        }
+    }
+};
+
+} // namespace graph_tool
+
+
+#endif // GRAPH_FDP_HH
+
+
